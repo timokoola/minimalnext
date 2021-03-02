c51fb4287be6f09491366777208ce8a7d6704427
Timo Eemeli Koola <timo@timokoola.com>
2021-03-02 19:44:28 +0200

    base next, still needs clean up

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Skadi Next Template</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Contact</li>
+            <li>Links</li>
+            <li>About</li>
+          </ul>
+        </nav>
+      </header>
+
+      <main className={styles.main}>
+        <p>Hep</p>
+      </main>
+
+      <footer className={styles.footer}>
+        <p className="color-quinary | text-major">Hop</p>
+      </footer>
+    </div>
+  );
+}
